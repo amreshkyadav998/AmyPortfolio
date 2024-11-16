3311690cf97de1fec806ab64283d99e4b220b000
Amresh Yadav <amreshky998@gmail.com>
2024-11-16 22:51:12 +0530

    header-fixed

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -3,12 +3,11 @@ import {AiOutlineTwitter} from "react-icons/ai";
 import {AiOutlineLinkedin} from "react-icons/ai";
 import { SiLeetcode, SiCodeforces } from "react-icons/si";
 
-const data = [
-
-    {id: 2, link: 'https://x.com/amreshky997?s=08', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://www.linkedin.com/in/amresh-yadav-223656257', icon: <AiOutlineLinkedin/>},
-    {id: 4, link: 'https://github.com/amreshkyadav998', icon: <AiFillGithub/>},
-    {id:5, link: 'http://leetcode.com/amreshky/', icon: <SiLeetcode/> },
+const data=[
+    {id:1,link:"https://github.com/amreshkyadav998",icon:<AiOutlineGithub />},
+    {id:2,link:"https://x.com/amreshky997?s=08",icon:<AiOutlineTwitter />},
+    {id:3,link:"https://www.linkedin.com/in/amresh-yadav-223656257",icon:<AiOutlineLinkedin />},
+    {id:4, link: 'http://leetcode.com/amreshky/', icon: <SiLeetcode/> },
     {id:5, link: 'https://codeforces.com/profile/amy999', icon: <SiCodeforces/> }
 ]
 
